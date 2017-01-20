cae6a8cda05ff325d7ae2a6b521958ff2369da4d
Stefany Kabatova <stefkabatova@gmail.com>
2017-01-20 10:37:51 -0500

    Added onclick eventhandler to game tiles

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ console.log("hello")
 var min = 1;
 var max = 11;
 var button = $("#initiator");
+var clickled_tile = $(".gameTile")
 
 
 function randomTile(min,max){
     var tile = Math.floor(Math.random() * (max - min) + min)
     console.log(tile)
 }
+
+
+clickled_tile.on("click", function(){
+  $(this).addClass('on');
+})
+
 
 button.on('click',function(){
   console.log("I work");

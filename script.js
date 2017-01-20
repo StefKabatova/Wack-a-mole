7afb952d798a8bb3101415a71d7b5a64891b8035
Stefany Kabatova <stefkabatova@gmail.com>
2017-01-20 10:23:02 -0500

    Created function that gives a random tile number

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
+$(document).ready(function() {
+  // test function to ensure connection to CDN link
+console.log("hello")
 
-console.log("hello");
+var min = 1;
+var max = 11;
+var button = $("#initiator");
+
+
+function randomTile(min,max){
+    var tile = Math.floor(Math.random() * (max - min) + min)
+    console.log(tile)
+}
+
+button.on('click',function(){
+  console.log("I work");
+});
+
+
+randomTile(min,max);
+
+});

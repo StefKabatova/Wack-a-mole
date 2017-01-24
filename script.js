314241d16e952228ebdc0e19d708b501af794445
Stefany Kabatova <stefkabatova@gmail.com>
2017-01-24 10:38:26 -0500

    fixed point allocation bug and changed tile background

--- script.js
+++ script.js
@@ -17,25 +17,30 @@ var timer ;
     min = 1;
     max = 11;
     var tilenumb = Math.floor(Math.random() * (max - min) + min);
-        clickledTile.eq(parseInt(tilenumb)).addClass('on');
+        clickledTile.eq(parseInt(tilenumb)).toggleClass('on');
       };
 
     // Initiates game and sets timer
     startButton.on('click', function(){
-        timer= setInterval(randomTile, 1000)
+      clickledTile.addClass('off');
+      timer= setInterval(randomTile, 1000)
 
     });
 
     //Use the "this" keyword to identify the object in function
     clickledTile.on('click', function(){
+      if($(this).hasClass('on')){
       scoreTotal += 100
       score.text(parseInt(scoreTotal))
       $(this).removeClass('on')
+    }
     })
 
     // stops set interval and stops game
     stopButton.on('click', function(){
       clearInterval(timer);
+      scoreTotal = 0;
+      score.text(parseInt(scoreTotal))
     })
 
 

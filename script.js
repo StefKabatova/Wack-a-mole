c83246e5900bc4dd3f2a9b6e47ab2245ecc7cd7e
Stefany Kabatova <stefkabatova@gmail.com>
2017-02-03 12:44:07 -0500

    fixed display of final score

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var min;
 var max;
 var score = $('#score');
 var scoreTotal = 0;
+var roundCounter = 0;
+var minusPoints = $('#minusPoint');
 var startButton = $("#start");
 var stopButton = $("#stop");
 var clickledTile = $(".gameTile");
@@ -17,30 +19,47 @@ var timer ;
     min = 1;
     max = 11;
     var tilenumb = Math.floor(Math.random() * (max - min) + min);
-        clickledTile.eq(parseInt(tilenumb)).toggleClass('on');
+
+    if (roundCounter > 5 && (tilenumb%2 == 0)){
+          clickledTile.eq(parseInt(tilenumb)).toggleClass('bad');
+
+
+
+        }
+          clickledTile.eq(parseInt(tilenumb)).toggleClass('on');
+          // clickledTile.eq(parseInt(tilenumb)).removeClass('bad')
+
       };
 
     // Initiates game and sets timer
     startButton.on('click', function(){
+
       clickledTile.addClass('off');
-      timer= setInterval(randomTile, 1000)
+      timer= setInterval(randomTile, 500)
 
     });
 
     //Use the "this" keyword to identify the object in function
     clickledTile.on('click', function(){
+      roundCounter += 1;
       if($(this).hasClass('on')){
       scoreTotal += 100
-      score.text(parseInt(scoreTotal))
+      score.text(parseInt(scoreTotal)).fadeOut().fadeIn()
       $(this).removeClass('on')
     }
+    if($(this).hasClass('bad')){
+    scoreTotal -= 50
+    score.text(parseInt(scoreTotal)).fadeOut().fadeIn()
+    $(this).removeClass('bad')
+  }
+
     })
 
     // stops set interval and stops game
     stopButton.on('click', function(){
       clearInterval(timer);
-      scoreTotal = 0;
-      score.text(parseInt(scoreTotal))
+      // scoreTotal = 0;
+      score.text("Total" + " " +  parseInt(scoreTotal))
     })
 
 
